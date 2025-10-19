80c248b0760f35d571f4107ab4169f33df3ee1d7
Rajo-Lahatra <lahatrajo@gmail.com>
2025-10-19 18:39:27 +0000

    désactivation middeleware.js

--- middleware.js
+++ middleware.js
@@ -1,28 +1,10 @@
-// Fichier : middleware.js
-
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+// middleware.js
 import { NextResponse } from 'next/server'
 
-export async function middleware(req) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
-
-  const { data: { session } } = await supabase.auth.getSession()
-  const { pathname } = req.nextUrl
-
-  // Rediriger vers /auth si non connecté et accès à la page principale
-  if (!session && pathname === '/') {
-    return NextResponse.redirect(new URL('/auth', req.url))
-  }
-
-  // Rediriger vers / si connecté et sur /auth
-  if (session && pathname === '/auth') {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
-  return res
+export async function middleware(request) {
+  return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/', '/auth'],
+  matcher: '/',
 }

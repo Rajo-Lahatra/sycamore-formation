56c0bd3e612299b945724a599e6a5601977b01dc
Rajo-Lahatra <lahatrajo@gmail.com>
2025-10-23 19:46:19 +0000

    dhfr

---
+++ src/app/Export/test/page.js
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div>
+      <h1>✅ TEST RÉUSSI</h1>
+      <p>Si vous voyez ce message, la route /export/test fonctionne !</p>
+      <p>Allez sur : <a href="/export/test">/export/test</a></p>
+    </div>
+  );
+}
